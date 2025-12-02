23ae02e5090661abf6bb483b993e75059ce14ee4
Sahil Singh <sahil171759@gmail.com>
2025-12-02 20:56:48 +0530

    fix: prevent 404 on reload by adding vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+  }
 })
