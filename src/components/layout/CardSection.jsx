d7dcff773190846f575d0f4c0a8f9642c8193ef7
sahil singh <sahil171759@gmail.com>
2025-12-02 22:54:00 +0530

    Update alt attribute for card image

--- src/components/layout/CardSection.jsx
+++ src/components/layout/CardSection.jsx
@@ -77,6 +77,7 @@ const CardSection = () => {
                 <motion.img
                   layoutId={`card-image-${meal.idMeal}`}
                   src={meal.strMealThumb}
+                  alt={meal.strTags}
                   className="w-full h-90 md:h-full shadow-sm object-cover"
                 />
                 <motion.div
